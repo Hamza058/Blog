4b92a91e720308f3ceab8aa64485977eef0ab03b
hamza kılıç <hamza2358@hotmail.com>
2023-04-11 16:03:27 +0300

    add login and some fixes

---
+++ Blog/Controllers/LoginController.cs
@@ -0,0 +1,32 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers
+{
+    public class LoginController : Controller
+    {
+        AdminManager am = new AdminManager(new EFAdminDal());
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Admin admin)
+        {
+            var value = am.TGetList().Where(x => x.AdminName == admin.AdminName && x.AdminPassword == admin.AdminPassword);
+            if (value.Any())
+            {
+                return RedirectToAction("Index","Home");
+            }
+            else
+            {
+                ViewBag.Message = "Wrong username or password";
+                return View();
+            }
+        }
+    }
+}
